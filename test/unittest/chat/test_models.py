d7e3335f0277eee86d441086d7e45e535391d0b0
YuriyLisovskiy <yuralisovskiy98@gmail.com>
2018-02-01 20:46:05 +0200

    added tests for Message model

--- test/unittest/chat/test_models.py
+++ test/unittest/chat/test_models.py
@@ -3,7 +3,6 @@ import glob
 
 from django.test import TestCase
 from django.core.files.images import ImageFile
-from django.utils.datetime_safe import datetime
 
 from account.models import UserProfile
 from chat.models import Message, ChatRoom
@@ -123,4 +122,187 @@ class TestChatRoom(TestCase):
 
 
 class TestMessage(TestCase):
-	pass
+	
+	def setUp(self):
+		data = {
+			'first_name': 'some_first_name',
+			'last_name': 'some_last_name',
+			'username': 'some_username',
+			'email': 'some.email@gmail.com',
+			'password': 'super_safe_password',
+		}
+		self.user1 = UserProfile.add(**data)
+		self.user1.logo = self.IMAGE_FILE()
+		self.user1.save()
+		data = {
+			'first_name': 'another_first_name',
+			'last_name': 'another_last_name',
+			'username': 'another_username',
+			'email': 'another.email@gmail.com',
+			'password': 'super_safe_password',
+			'photo': self.IMAGE_FILE('temp.png')
+		}
+		self.user2 = UserProfile.add(**data)
+		self.user2.logo = self.IMAGE_FILE('test.png')
+		self.user2.save()
+		data = {
+			'author': self.user2,
+			'friend': self.user1,
+			'logo': self.user2.logo
+		}
+		self.ch_room = ChatRoom.add(**data)
+		message_data = {
+			'chat_room': self.ch_room,
+			'author': self.user2,
+			'msg': 'Some message text',
+			'time': 'some time'
+		}
+		self.msg_id = Message.add(**message_data).id
+	
+	def test_add(self):
+		message_data = {
+			'chat_room': self.ch_room,
+			'author': self.user1,
+			'msg': 'Some message',
+			'time': 'time',
+		}
+		msg_id = Message.add(**message_data).id
+		message = Message.get_by_id(msg_id)
+		self.assertEqual(message.chat_room, self.ch_room)
+		self.assertEqual(message.msg, 'Some message')
+		self.assertEqual(message.author_username, self.user1.username)
+		self.assertEqual(message.time, 'time')
+		self.assertEqual(message.author_fn_ln, self.user1.first_name[0] + self.user1.last_name[0])
+		self.assertEqual(message.author_logo, self.user1.logo)
+		self.assertEqual(message.author_id, str(self.user1.id))
+	
+	def test_edit(self):
+		message = Message.get_by_id(self.msg_id)
+		self.assertEqual(message.chat_room, self.ch_room)
+		self.assertEqual(message.msg, 'Some message text')
+		self.assertEqual(message.author_username, self.user2.username)
+		self.assertEqual(message.time, 'some time')
+		self.assertEqual(message.author_fn_ln, self.user2.first_name[0] + self.user2.last_name[0])
+		self.assertEqual(message.author_logo, self.user2.logo)
+		self.assertEqual(message.author_id, str(self.user2.id))
+		message_data = {
+			'chat_room': self.ch_room,
+			'author': self.user1,
+			'msg': 'Some message',
+			'time': 'time',
+		}
+		message = Message.edit(pk=100500, **message_data)
+		self.assertEqual(message, None)
+		Message.edit(pk=self.msg_id, **message_data)
+		message = Message.get_by_id(self.msg_id)
+		self.assertEqual(message.chat_room, self.ch_room)
+		self.assertEqual(message.msg, 'Some message')
+		self.assertEqual(message.author_username, self.user1.username)
+		self.assertEqual(message.time, 'time')
+		self.assertEqual(message.author_fn_ln, self.user1.first_name[0] + self.user1.last_name[0])
+		self.assertEqual(message.author_logo, self.user1.logo)
+		self.assertEqual(message.author_id, str(self.user1.id))
+	
+	def test_filter_by(self):
+		message_data = {
+			'chat_room': self.ch_room,
+			'author': self.user1,
+			'msg': 'Message text',
+			'time': 'some time',
+		}
+		Message.add(**message_data)
+		filter_data = {
+			'chat_room': self.ch_room
+		}
+		messages = Message.filter_by(**filter_data)
+		self.assertEqual(len(messages), 2)
+		filter_data = {
+			'msg': 'Message text'
+		}
+		messages = Message.filter_by(**filter_data)
+		self.assertEqual(len(messages), 1)
+		filter_data = {
+			'author': self.user1
+		}
+		messages = Message.filter_by(**filter_data)
+		self.assertEqual(len(messages), 1)
+		filter_data = {
+			'time': 'some time'
+		}
+		messages = Message.filter_by(**filter_data)
+		self.assertEqual(len(messages), 2)
+		filter_data = {
+			'author_fn_ln': self.user1.first_name[0] + self.user1.last_name[0]
+		}
+		messages = Message.filter_by(**filter_data)
+		self.assertEqual(len(messages), 1)
+		filter_data = {
+			'author_logo': self.user1.logo
+		}
+		messages = Message.filter_by(**filter_data)
+		self.assertEqual(len(messages), 1)
+		filter_data = {
+			'author_id': self.user2.id
+		}
+		messages = Message.filter_by(**filter_data)
+		self.assertEqual(len(messages), 1)
+		filter_data = {
+			'time': 'interesting time'
+		}
+		messages = Message.filter_by(**filter_data)
+		self.assertEqual(len(messages), 0)
+	
+	def test_get_all(self):
+		messages = Message.get_all()
+		self.assertEqual(len(messages), 1)
+		message_data = {
+			'chat_room': self.ch_room,
+			'author': self.user1,
+			'msg': 'Some message',
+			'time': 'time',
+		}
+		msg_id = Message.add(**message_data).id
+		messages = Message.get_all()
+		self.assertEqual(len(messages), 2)
+		Message.remove(msg_id)
+		Message.remove(self.msg_id)
+		messages = Message.get_all()
+		self.assertEqual(len(messages), 0)
+	
+	def test_to_dict(self):
+		message = Message.get_by_id(self.msg_id)
+		message_dict = message.to_dict()
+		self.assertEqual(message_dict['id'], self.msg_id)
+		self.assertEqual(message_dict['chat_room'], self.ch_room.id)
+		self.assertEqual(message_dict['msg'], 'Some message text')
+		self.assertEqual(message_dict['author_username'], self.user2.username)
+		self.assertEqual(message_dict['time'], 'some time')
+		self.assertEqual(message_dict['author_fn_ln'], self.user2.first_name[0] + self.user2.last_name[0])
+		self.assertEqual(message_dict['author_id'], str(self.user2.id))
+		self.assertEqual(message_dict['author_logo'], self.user2.logo.url)
+	
+	def test_remove(self):
+		Message.remove(self.msg_id)
+		message = Message.get_by_id(self.msg_id)
+		self.assertEqual(message, None)
+		Message.remove(100500)
+	
+	def tearDown(self):
+		try:
+			os.remove('media/test.png')
+		except OSError:
+			pass
+		try:
+			os.remove('media/temp.png')
+		except OSError:
+			pass
+		for file in glob.glob('media/test_*.png'):
+			os.remove(file)
+		for file in glob.glob('media/temp_*.png'):
+			os.remove(file)
+	
+	@staticmethod
+	def IMAGE_FILE(name='test.png'):
+		img = ImageFile(open(name, 'w+'), name)
+		os.remove(name)
+		return img
