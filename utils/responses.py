121daf7dc695462b8dc7a49451be1eac68bd2429
YuriyLisovskiy <yuralisovskiy98@gmail.com>
2018-02-01 21:04:10 +0200

    updated utils/responses.py

--- utils/responses.py
+++ utils/responses.py
@@ -6,24 +6,36 @@ CREATED = HttpResponse('created', status=201)
 
 
 def BAD_REQUEST():
-	return render_to_response('error.html', {
-		'error_code': 400,
-		'error_type': 'Bad request',
-		'error_message': 'Bad request'
-	})
+	return render_to_response(
+		'error.html',
+		status=400,
+		context={
+			'error_code': 400,
+			'error_type': 'Bad request',
+			'error_message': 'Bad request'
+		}
+	)
 
 
 def PERMISSION_DENIED():
-	return render_to_response('error.html', {
-		'error_code': 403,
-		'error_type': 'Forbidden',
-		'error_message': 'Access denied'
-	})
+	return render_to_response(
+		'error.html',
+		status=403,
+		context={
+			'error_code': 403,
+			'error_type': 'Forbidden',
+			'error_message': 'Access denied'
+		}
+	)
 
 
 def NOT_FOUND():
-	return render_to_response('error.html', {
-		'error_code': 404,
-		'error_type': 'Not found',
-		'error_message': 'The web page does not exist'
-	})
+	return render_to_response(
+		'error.html',
+		status=404,
+		context={
+			'error_code': 404,
+			'error_type': 'Not found',
+			'error_message': 'The web page does not exist'
+		}
+	)
