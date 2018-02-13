fd176e1117429c0d5c84c37af72c3b4e045f31e0
YuriyLisovskiy <yuralisovskiy98@gmail.com>
2018-02-13 22:50:52 +0200

    updated view modifiers

--- utils/view_modifiers.py
+++ utils/view_modifiers.py
@@ -1,13 +1,9 @@
-from django.shortcuts import redirect
-
 from .responses import PERMISSION_DENIED
-from messenger.settings import LOGIN_REDIRECT_URL
 
 
 def sudo_required(func):
 	def wrapper(*args, **kwargs):
 		request = None
-
 		if 'request' in kwargs.keys():
 			request = kwargs['request']
 		elif 'request' in func.__code__.co_varnames:
@@ -16,21 +12,15 @@ def sudo_required(func):
 		elif 'self' in func.__code__.co_varnames:
 			if 'request' in vars(args[0]):
 				request = func.__self__.request
-
 		if request and not request.user.is_superuser:
-			if not request.user.is_authenticated:
-				return redirect(LOGIN_REDIRECT_URL)
-			else:
-				return PERMISSION_DENIED
+			return PERMISSION_DENIED
 		return func(*args, **kwargs)
-
 	return wrapper
 
 
 def auth_required(func):
 	def wrapper(*args, **kwargs):
 		request = None
-
 		if 'request' in kwargs.keys():
 			request = kwargs['request']
 		elif 'request' in func.__code__.co_varnames:
@@ -39,10 +29,9 @@ def auth_required(func):
 		elif 'self' in func.__code__.co_varnames:
 			if 'request' in vars(args[0]):
 				request = func.__self__.request
-
 		if request:
 			if not request.user.is_authenticated:
-				return redirect(LOGIN_REDIRECT_URL)
+				return PERMISSION_DENIED
 		return func(*args, **kwargs)
 
 	return wrapper
